5454d4b32b96dd11d35724bdbb14b4f768e588f2
Jason Whitted <jason.whitted@clover.com>
2019-10-08 10:43:33 -0600

    Update from boilerplate push

--- server/index.js
+++ server/index.js
@@ -6,15 +6,15 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 
-app.all('/hello', (req, res) =>
+app.all('/hello', (req, res) => {
   res.json({
     hello: 'world',
     when: new Date().getTime(),
     method: req.method,
     body: req.body,
     query: req.query,
-  })
-);
+  });
+});
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
