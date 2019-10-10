f58efa48df6dd448bfa35f9c45bbb0b204703a4b
Jason Whitted <jason.whitted@clover.com>
2019-10-10 12:23:44 -0600

    Convert SuiteList class->function

--- src/components/SuitesList/SuitesList.js
+++ src/components/SuitesList/SuitesList.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Alert, Card, CardHeader, CardBody, ListGroup, ListGroupItem } from 'reactstrap';
@@ -8,63 +8,52 @@ import { Loading } from '../Loading';
 import SuiteItem from './SuiteItem';
 import './stylesheet.css';
 
-class SuitesList extends Component {
-  componentDidMount() {
-    const { getSuites } = this.props;
+const SuitesList = ({ getSuites, loading, message, names, selected, setSelectedSuite, status }) => {
+  useEffect(() => {
     getSuites();
-  }
+  }, [getSuites]);
 
-  refresh = () => {
-    const { getSuites } = this.props;
-    getSuites({ force: true });
-  };
+  const refresh = () => getSuites({ force: true });
+  const select = name => setSelectedSuite(name);
 
-  select = name => {
-    const { setSelectedSuite } = this.props;
-    setSelectedSuite(name);
-  };
-
-  render() {
-    const { loading, message, names, selected, status } = this.props;
-    return (
-      <Card className="SuitesList" color="secondary" inverse>
-        <CardHeader>
-          Test Suites {!loading && <span className="text-dark small">({names.length})</span>}
-          <button type="button" className="btn btn-secondary btn-sm p-0 px-1" onClick={this.refresh} title="Refresh">
-            <FontAwesomeIcon icon="sync" />
-          </button>
-        </CardHeader>
-        {loading && (
-          <CardBody>
-            <Loading />
-          </CardBody>
-        )}
-        {!loading && (
-          <CardBody className="m-0 p-0">
-            {(status !== 'success' || !!message) && (
-              <Alert color="warning" className="mb-0">
-                <FontAwesomeIcon icon="info-circle" />
-                <span className="ml-2">
-                  {status}. {message}
-                </span>
-              </Alert>
+  return (
+    <Card className="SuitesList" color="secondary" inverse>
+      <CardHeader>
+        Test Suites {!loading && <span className="text-dark small">({names.length})</span>}
+        <button type="button" className="btn btn-secondary btn-sm p-0 px-1" onClick={refresh} title="Refresh">
+          <FontAwesomeIcon icon="sync" />
+        </button>
+      </CardHeader>
+      {loading && (
+        <CardBody>
+          <Loading />
+        </CardBody>
+      )}
+      {!loading && (
+        <CardBody className="m-0 p-0">
+          {(status !== 'success' || !!message) && (
+            <Alert color="warning" className="mb-0">
+              <FontAwesomeIcon icon="info-circle" />
+              <span className="ml-2">
+                {status}. {message}
+              </span>
+            </Alert>
+          )}
+          <ListGroup>
+            {!names.length && (
+              <ListGroupItem>
+                <i>Nothing to display</i>
+              </ListGroupItem>
             )}
-            <ListGroup>
-              {!names.length && (
-                <ListGroupItem>
-                  <i>Nothing to display</i>
-                </ListGroupItem>
-              )}
-              {names.map(name => (
-                <SuiteItem key={name} active={selected === name} name={name} onClick={this.select} />
-              ))}
-            </ListGroup>
-          </CardBody>
-        )}
-      </Card>
-    );
-  }
-}
+            {names.map(name => (
+              <SuiteItem key={name} active={selected === name} name={name} onClick={select} />
+            ))}
+          </ListGroup>
+        </CardBody>
+      )}
+    </Card>
+  );
+};
 
 SuitesList.propTypes = {
   // connect
